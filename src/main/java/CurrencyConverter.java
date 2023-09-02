d601a89439c5fd62e09999a39886c9413f93b8f5
SergioGL <sgl.sergio.garcia@gmail.com>
2023-09-02 04:01:08 -0600

    CurrencyConverter done

--- src/main/java/CurrencyConverter.java
+++ src/main/java/CurrencyConverter.java
@@ -1,6 +1,5 @@
 /*
-
-*  PROYECTOS EN JAVA PARA EL CÍRCULO DE ESTUDIO
+*  PROYECTOS EN JAVA PARA EL CIRCULO DE ESTUDIO SESION 09 MODULO 01     AGOSTO-SEPTIEMBRE 2023
 * * Currency Converter
 
  "La idea es desarrollar un sistema interactivo de conversión
@@ -19,42 +18,117 @@
 * */
 
 import java.util.Map;
+import java.util.Scanner;
 import java.util.TreeMap;
 
-/*
-REQ+ : Anotar justificaciones e.g. Se empleó List porque ... [descripción List]
-*/
 public class CurrencyConverter {
+    static Map<Integer, String> divisasAbbrev = new TreeMap<>();
+    static Map<Integer, Double> divisasValores = new TreeMap<>();//MSN Money, Saturday 02:52 AM 09/02/2023
+    static int selectedDivisaOrigen;
+    static int selectedDivisaDestino;
+    static double selectedCantidad;
+
     public static void main(String[] args) {
+        divisasAbbrev.put(1, "MXN"); // Peso mexicano
+        divisasAbbrev.put(2, "USD"); // Dólar estadounidense
+        divisasAbbrev.put(3, "CAD"); // Dólar canadiense
+        divisasAbbrev.put(4, "EUR"); // Euro
+        divisasAbbrev.put(5, "GBP"); // Libra esterlina
+        divisasAbbrev.put(6, "JPY"); // Yen japonés
+        divisasAbbrev.put(7, "CHF"); // Franco suizo
+        divisasAbbrev.put(8, "AUD"); // Dólar australiano
+        divisasAbbrev.put(9, "NZD"); // Dólar neozelandés
+        divisasAbbrev.put(10, "BRL"); // Real brasileño
+
+        divisasValores.put(1, 17.08);// "MXN"); // Peso mexicano
+        divisasValores.put(2, 1.0);// "USD"); // Dólar estadounidense
+        divisasValores.put(3, 1.36);// "CAD"); // Dólar canadiense
+        divisasValores.put(4, 0.93);// "EUR"); // Euro
+        divisasValores.put(5, 0.79);// "GBP"); // Libra esterlina
+        divisasValores.put(6, 146.23);// "JPY"); // Yen japonés
+        divisasValores.put(7, 0.89);// "CHF"); // Franco suizo
+        divisasValores.put(8, 1.55);// "AUD"); // Dólar australiano
+        divisasValores.put(9, 1.68);// "NZD"); // Dólar neozelandés
+        divisasValores.put(10, 4.95);//  "BRL"); // Real brasileño
         run();
     }
 
-    private static String run() {
-        String SEP = "* * * * * * * * * *";
-        boolean salir = false;
+    private static void run() {
+        int divisaInt = 1;
+        System.out.println("""
+                * * * * * * * * * *
+                Conversor Divisas
 
-        Map<Integer, String> divisas = new TreeMap<>();
-        divisas.put(1, "MX");
-        divisas.put(1, "MX");
-     divisas.put(1, "MXN"); // Peso mexicano
-divisas.put(2, "USD"); // Dólar estadounidense
-divisas.put(3, "CAD"); // Dólar canadiense
-
-divisas.put(4, "EUR"); // Euro
-divisas.put(5, "GBP"); // Libra esterlina
-divisas.put(6, "JPY"); // Yen japonés
-divisas.put(7, "CHF"); // Franco suizo
-divisas.put(8, "AUD"); // Dólar australiano
-divisas.put(9, "NZD"); // Dólar neozelandés
-divisas.put(10, "BRL"); // Real brasileño
-
-        do {
-            System.out.println(SEP);
-            System.out.println("\nCurrency Converter\n\nDivisas:\n\n");
-
+                Divisas:
+                """);
+        for (String divisaOrigen : divisasAbbrev.values()) {
+            System.out.printf("%d\t%s\n", divisaInt, divisaOrigen);
+            divisaInt++;
         }
-        while (!salir);
 
-        return SEP;
+        selectedDivisaOrigen = askCurrency("Selecciona divisa origen[1-10]:");
+        selectedDivisaDestino = askCurrency("Selecciona divisa destino[1-10]:");
+
+        System.out.printf("\nDetermine la cantidad a convertir de %s a %s:", divisasAbbrev.get(selectedDivisaOrigen), divisasAbbrev.get(selectedDivisaDestino));
+        selectedCantidad = askDouble();
+
+        printConversion();
+
+        askNextAction();
+    }
+
+
+    private static int askInteger() {
+        Scanner sc = new Scanner(System.in);
+        return sc.hasNextInt() ? sc.nextInt() : askInteger(0);
+    }
+
+    @SuppressWarnings("unused")
+    private static int askInteger(int i) {
+        System.out.println("Entrada no valida intenta de nuevo:");
+        return askInteger();
+    }
+
+    private static double askDouble() {
+        Scanner sc = new Scanner(System.in);
+        return sc.hasNextDouble() ? sc.nextDouble() : askDouble(0);
+    }
+
+    @SuppressWarnings("unused")
+    private static double askDouble(double i) {
+        System.out.println("Entrada no valida intenta de nuevo:");
+        return askDouble();
+    }
+
+    private static int askCurrency(String println) {
+        System.out.println(println);
+        int i = askInteger();
+        if (i < 1 || i > 10) {
+            System.out.println("Fuera de rango");
+            return askCurrency(println);
+        } else return i;
+    }
+
+    private static double convertCurrency(double cantidad, int divOrigen, int divDestino) {
+        return (cantidad / divisasValores.get(divOrigen)) * divisasValores.get(divDestino);
+    }
+
+    private static void printConversion() {
+        System.out.printf("\n%.2f %s = %.2f %s:", selectedCantidad, divisasAbbrev.get(selectedDivisaOrigen), convertCurrency(selectedCantidad, selectedDivisaOrigen, selectedDivisaDestino), divisasAbbrev.get(selectedDivisaDestino));
+    }
+
+    private static void askNextAction() {
+        System.out.println("\n\n¿Qué desea hacer a continuación?\n[1] Nueva conversión\n[2] Nueva divisa destino\n[3] Salir");
+        switch (askInteger()) {
+            case 1 -> run();
+            case 2 -> newConversion();
+            default -> System.exit(0);
+        }
+    }
+
+    private static void newConversion() {
+        selectedDivisaDestino = askCurrency("Selecciona divisa destino[1-10]:");
+        printConversion();
+        askNextAction();
     }
 }
